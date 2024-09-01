85986f51c3efc74b1b5d8c684865b771e95f31d8
Olgasyla <olgasyla@gmail.com>
2024-09-01 21:51:34 +0200

    Add unitTest to findTransactionById, saveNewTransaction, updateTransaction,deleteTransaction

--- backend/src/test/java/org/example/backend/TransactionServiceTest.java
+++ backend/src/test/java/org/example/backend/TransactionServiceTest.java
@@ -1,9 +1,6 @@
 package org.example.backend;
 
-import org.example.backend.model.Account;
-import org.example.backend.model.Category;
-import org.example.backend.model.Transaction;
-import org.example.backend.model.TransactionType;
+import org.example.backend.model.*;
 import org.example.backend.repository.TransactionRepository;
 import org.example.backend.service.IdService;
 import org.example.backend.service.TransactionService;
@@ -11,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -40,18 +38,60 @@ class TransactionServiceTest {
     }
 
     @Test
-    void findTransactionById() {
+    void findTransactionByIdTest() {
+        //GIVEN
+        Transaction transaction = new Transaction("3", "Food", localDate, 52.5, Account.BANK, "Aldi", Category.FOOD, TransactionType.EXPENSE);
+        when(transactionRepository.findById("3")).thenReturn(Optional.of(transaction));
+        //WHEN
+        Transaction actual = transactionService.findTransactionById("3");
+        //THEN
+        Transaction expected = new Transaction("3", "Food", localDate, 52.5, Account.BANK, "Aldi", Category.FOOD, TransactionType.EXPENSE);
+        verify(transactionRepository).findById("3");
+        assertEquals(expected, actual);
     }
 
     @Test
-    void saveNewTransaction() {
+    void saveNewTransactionTest() {
+        //GIVEN
+        //WHEN
+        TransactionDto transactionDto = new TransactionDto("Kino", localDate, 12.5, Account.WALLET, "Cinema", Category.ENTERTAINMENT, TransactionType.EXPENSE);
+        Transaction toSave = new Transaction("4", transactionDto.name(),
+                transactionDto.date(), transactionDto.amount(), transactionDto.account(), transactionDto.description(),
+                transactionDto.category(),transactionDto.type());
+        when(transactionRepository.save(toSave)).thenReturn(toSave);
+        when(idService.randomId()).thenReturn(toSave.id());
+
+        //WHEN
+        Transaction actual = transactionService.saveNewTransaction(transactionDto);
+        //THEN
+        Transaction expected = new Transaction("4", transactionDto.name(),transactionDto.date(), transactionDto.amount(), transactionDto.account(), transactionDto.description(),
+                transactionDto.category(),transactionDto.type());
+        verify(transactionRepository).save(toSave);
+        verify(idService).randomId();
+        assertEquals(expected, actual);
     }
 
     @Test
-    void updateTransaction() {
+    void updateTransactionTest() {
+        //GIVEN
+        String id = "4";
+        TransactionDto transactionDto = new TransactionDto("Kino", localDate, 12.5, Account.WALLET, "Cinema", Category.ENTERTAINMENT, TransactionType.EXPENSE);
+        Transaction transaction = new Transaction(id,"Kino", localDate, 12.5, Account.WALLET, "Cinema", Category.ENTERTAINMENT, TransactionType.EXPENSE);
+        when(transactionRepository.findById(id)).thenReturn(Optional.of(transaction));
+        when(transactionRepository.save(transaction)).thenReturn(transaction);
+
+        //WHEN
+        Transaction actual = transactionService.updateTransaction(id, transactionDto);
+
+        //THEN
+        verify(transactionRepository).findById(id);
+        verify(transactionRepository).save(transaction);
+        assertEquals(transaction, actual);
     }
 
     @Test
-    void deleteTransaction() {
-    }
-}
+    void deleteTransactionTest() {
+        doNothing().when(transactionRepository).deleteById("2");
+        transactionRepository.deleteById("2");
+        verify(transactionRepository).deleteById("2");
+    }}
